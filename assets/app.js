c487eb595a32c9985546d9ed80a73519430d2812
Abdus Sabur <saburali7264@gmail.com>
2021-09-19 18:05:29 +0600

    Input Box - Type Anything & Press Enter -> Get Input Value

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,18 @@
+// Button Click Get Input Value
 const bookSearch = () => {
     const searchField = document.getElementById("searchBook");
     const searchText = searchField.value;
     const url = `https://www.openlibrary.org/search.json?q=${searchText}`;
-    console.log(url);
 }
+
+// Hit Enter Get Search Value
+const enterBtnAction = () => {
+    let input = document.getElementById("searchBook");
+    input.addEventListener("keyup", function (event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            document.getElementById("searchBtn").click();
+        }
+    });
+}
+enterBtnAction();
